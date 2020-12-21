f3cf74b05364e4ad1705dca80069337748e62ce5
ochukochris <chrisochuko101@gmail.com>
2020-12-21 03:51:12 +0100

    changed routes naming style

--- routes/users.js
+++ routes/users.js
@@ -2,16 +2,20 @@ const express = require('express')
 const router = express.Router()
 const UserController = require('../app/controllers/UserController')
 
+// middlewares
 const validateUser = require('../app/middlewares/validators/user/validateUser')
 
-/* GET users listing. */
-router.get('/get_users', UserController.findAll)
-/* GET a user. */
-router.get('/get_user', UserController.findOne)
+/**
+ *  Main routes 
+ * **/
 
 /* CREATE user. */
-router.post('/create_user', validateUser, UserController.create)
+router.post('/create', validateUser, UserController.create)
 /* LOGIN user. */
 router.post('/login', UserController.login)
+/* GET users listing. */
+router.get('/fetch', UserController.findAll)
+/* GET a user. */
+router.get('/get', UserController.findOne)
 
 module.exports = router
